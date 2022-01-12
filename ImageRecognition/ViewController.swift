61df17f2989d298aca4d6d252301d18c8f30d58f
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2022-01-12 23:15:31 +0200

    Incorporate InceptionV3 model and recognise different images taken

--- ImageRecognition/ViewController.swift
+++ ImageRecognition/ViewController.swift
@@ -26,9 +26,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             selectedImageView.image = pickedImage
+            guard let coreImage = CIImage(image: pickedImage) else {
+                fatalError("Unable to convert to CIImage")
+            }
+            detect(image: coreImage)
+            
         }
         
         pickerVeiw.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Unable to load CoreML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Unable to procces the image")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
